437901e9b60464f56e32f37b6534b21283402110
nplybon <plybonnj@dukes.jmu.edu>
2022-04-24 14:38:23 -0400

    server client no error checking

--- lab3client.c
+++ lab3client.c
@@ -1,19 +1,18 @@
 /*
-
     CS 361 Lab 3
     lab3client.c
-
-    Name: 
-
-    Honor Code Statement: 
-
+    Name: Nic Plybon & Adrien Ponce 
+    Honor Code Statement: This code complies with the JMU Honor Code
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include "network.h"
+#include <unistd.h>
+
 
 int main(int argc, char *argv[]) {
 
@@ -38,6 +37,40 @@ int main(int argc, char *argv[]) {
     }
 
     /* Implement the remaining client code here */
+    struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+
+
+    inet_pton(AF_INET, argv[1], &server_address.sin_addr);
+    server_address.sin_port = htons(server_port);
+
+    int sockfd;
+	if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		printf("could not create socket\n");
+		return 1;
+	}
+
+    const char* data_to_send = argv[3];
+    // send data
+	int len =
+	    sendto(sockfd, data_to_send, strlen(data_to_send), 0,
+	           (struct sockaddr*)&server_address, sizeof(server_address));
+
+	// received echoed data back
+	char buffer[100];
+	recvfrom(sockfd, buffer, len, 0, NULL, NULL);
+
+	buffer[len] = '\0';
+	printf("recieved: '%s'\n", buffer);
+
+	// close the socket
+	close(sockfd);
+
+
+
+
+
 
     return 0;
 
--- lab3server.c
+++ lab3server.c
@@ -1,23 +1,26 @@
 /*
-
     CS 361 Lab 3
     lab3server.c
-
-    Name: 
-
-    Honor Code Statement: 
-
+    Name: Nic Plybon & Adrien Ponce
+    Honor Code Statement: This code complies with the JMU Honor Code
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
 
 #include "network.h"
 
 int main(int argc, char *argv[]) {
 
     uint16_t req_listen_port;           // port number to listen for requests
-
 
     if (argc != 2) {
         printf("Usage: %s listen_port\n", argv[0]);
@@ -33,6 +36,53 @@ int main(int argc, char *argv[]) {
 
     /* Implement the remaining server code here */
 
+    // netstat -l -p | less
+    // nc -u 127.0.0.1 port msg
+
+    // creating socket file descriptor
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        printf("Error creating socket: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // populating server information
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(req_listen_port);
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    // bind the socket with the server address
+    if(bind(sockfd, (struct sockaddr *) &server_address, sizeof(struct sockaddr_in)) < 0) {
+        printf("Error binding to socket: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    // socket address used to store client address
+	struct sockaddr_in client_address;
+	int client_address_len = 0;
+
+	// run indefinitely
+	while (1) {
+		char buffer[500];
+
+		// read content into buffer from an incoming client
+		int len = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+		                   (struct sockaddr *)&client_address,
+		                   &client_address_len);
+
+		// inet_ntoa prints user friendly representation of the
+		// ip address
+		buffer[len] = '\0';
+		printf("received: '%s' from client %s\n", buffer,
+		       inet_ntoa(client_address.sin_addr));
+
+		// send same content back to the client ("echo")
+		sendto(sockfd, buffer, len, 0, (struct sockaddr *)&client_address,
+		       sizeof(client_address));
+    }
     return 0;
 
 }
